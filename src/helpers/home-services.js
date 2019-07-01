7bb5c51655deb05da889017f697ce04aa7ed3608
Victor Cleber Laureano <victor.laureano@gmail.com>
2019-06-30 22:53:53 -0300

    Fix Things

--- src/helpers/home-services.js
+++ src/helpers/home-services.js
@@ -3,32 +3,32 @@ import fetch from 'node-fetch'
 const url = 'http://10.0.0.2:8123/api/'
 const token = process.env.HASS_TOKEN
 const headers = {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json',
-    'Authorization': 'Bearer ' + token,
+  'Accept': 'application/json',
+  'Content-Type': 'application/json',
+  'Authorization': 'Bearer ' + token
 }
 
-async function callService(domain, action, entity_id, body) {
-    const response = await fetch(`${url}services/${domain}/${action}`, {
-        method: 'POST',
-        headers,
-        body: JSON.stringify({
-            entity_id,
-            ...body
-        })
+async function callService (domain, action, entityId, body) {
+  const response = await fetch(`${url}services/${domain}/${action}`, {
+    method: 'POST',
+    headers,
+    body: JSON.stringify({
+      entity_id: entityId,
+      ...body
     })
-    return await response.text()
+  })
+  return response.json()
 }
 
-async function getStatus(entity_id) {
-    const response = await fetch(`${url}states/${entity_id}`, {
-        method: 'GET',
-        headers,
-    })
-    return await response.json()
+async function getStatus (entityId) {
+  const response = await fetch(`${url}states/${entityId}`, {
+    method: 'GET',
+    headers
+  })
+  return response.json()
 }
 
 export default {
-    getStatus,
-    callService
+  getStatus,
+  callService
 }
--- src/modules/v1/vacuum/controller.js
+++ src/modules/v1/vacuum/controller.js
@@ -1,14 +1,14 @@
 import homeServices from '@helpers/home-services'
 
-const entity_id = "vacuum.xiaomi_vacuum_cleaner"
-const fan_speed_list = {
+const entityId = 'vacuum.xiaomi_vacuum_cleaner'
+const fanSpeedList = {
   quiet: 'Quiet',
   balanced: 'Balanced',
   turbo: 'Turbo',
   max: 'Max'
 }
 
-let alias_zones
+let aliasZones
 {
   let lucabedroom = [[24200, 27350, 26600, 31300]]
   let kitchen = [[21950, 33300, 26500, 35000]]
@@ -18,7 +18,7 @@ let alias_zones
     [23200, 26000, 24200, 26500],
     [22900, 23500, 26000, 26000]
   ]
-  alias_zones = {
+  aliasZones = {
     lucabedroom,
     kitchen,
     livingroom,
@@ -29,21 +29,21 @@ let alias_zones
   }
 }
 
-function getZone(name) {
+function getZone (name) {
   name = name.toLowerCase()
     .replace(/(up the |the |up my |my |s )/, '')
     .replace(/[^a-z]*/g, '')
   console.log(name)
-  return alias_zones[name]
+  return aliasZones[name]
 }
 
-async function getStatus() {
-  return await homeServices.getStatus(entity_id)
+async function getStatus () {
+  return homeServices.getStatus(entityId)
 }
 
-async function setSpeed(speed) {
-  return await homeServices.callService('vacuum', 'set_fan_speed', entity_id, {
-    fan_speed: fan_speed_list[speed.toLowerCase()]
+async function setSpeed (speed) {
+  return homeServices.callService('vacuum', 'set_fan_speed', entityId, {
+    fan_speed: fanSpeedList[speed.toLowerCase()]
   })
 }
 
@@ -51,48 +51,40 @@ export default {
   cleanZone: (req, res) => {
     const { zone, repeats, speed } = req.body
 
-    if (speed)
-      setSpeed(speed)
+    if (speed) { setSpeed(speed) }
 
-    homeServices.callService('vacuum', 'xiaomi_clean_zone', entity_id, {
-      "repeats": repeats,
-      "zone": getZone(zone)
+    homeServices.callService('vacuum', 'xiaomi_clean_zone', entityId, {
+      'repeats': repeats,
+      'zone': getZone(zone)
     }).then(response => {
       res.status(200).json({ status: `cleaning ${zone}` })
     })
-
   },
   stop: async (req, res) => {
-
     const status = await getStatus()
-    if (status.state == "returning")
-      await homeServices.callService('vacuum', 'pause', entity_id)
-//    else if (status.state == "docked" || status.state == "idle")
-//      return res.status(200).json({ status: 'stoped' })
+    if (status.state === 'returning') { await homeServices.callService('vacuum', 'pause', entityId) }
 
-    homeServices.callService('vacuum', 'stop', entity_id)
+    homeServices.callService('vacuum', 'stop', entityId)
       .then(response =>
         res.status(200).json({ status: 'stoping' })
       )
   },
   dock: (req, res) =>
-    homeServices.callService('vacuum', 'return_to_base', entity_id)
+    homeServices.callService('vacuum', 'return_to_base', entityId)
       .then(response =>
         res.status(200).json({ status: 'going back' })
-      )
-  ,
+      ),
   setSpeed: (req, res) => {
     const { speed } = req.body
 
-    setSpeed(speed).then(async response =>
-      res.status(200).json(await response.json())
+    setSpeed(speed).then(response =>
+      res.status(200).json(response)
     )
   },
   status: (req, res) =>
     getStatus().then(response =>
       res.status(200).json(response)
-    )
-  ,
+    ),
   zones: (req, res) =>
-    res.status(200).json(alias_zones)
+    res.status(200).json(aliasZones)
 }
