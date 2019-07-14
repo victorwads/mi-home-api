1c4da191d04ef1b8a915a0d7eeb80cfec494cf4f
Victor Wads <victor.laureano@gmail.com>
2019-07-14 00:59:30 -0300

    Create Notification Retry Quee

--- src/helpers/onesignal.js
+++ src/helpers/onesignal.js
@@ -4,21 +4,34 @@ const userAuthKey = process.env.ONE_SIGNAL_USER_KEY
 const appAuthKey = process.env.ONE_SIGNAL_APP_KEY
 const appId = "d1b284e5-8e85-4b00-bda9-09ae03553fc4"
 
-console.log('userAuthKey', userAuthKey)
-console.log('appAuthKey', appAuthKey)
-console.log('appId', appId)
+const NotificationsQuee = []
+let queePid = 0
+function sendQuee() {
+    if (NotificationsQuee.length === 0)
+        return
+    clearTimeout(queePid)
+    for (let i = 0; i < NotificationsQuee.length; i++) {
+        Client.sendNotification(NotificationsQuee[i])
+            .then(() => {
+                NotificationsQuee.slice(i, 1)
+            })
+            .catch(err => {
+                console.log(err.data)
+                NotificationsQuee.push(notification)
+            })
+    }
+    queePid = setTimeout(sendQuee, 2000)
+}
 
 export const Client = new OneSignal.Client({ userAuthKey, app: { appAuthKey, appId } });
 export const Notify = async (msg, url) => {
-    let notification = new OneSignal.Notification({
+    NotificationsQuee.push(new OneSignal.Notification({
         contents: {
             en: msg,
         },
         web_buttons: url ? [{ id: "see", text: "Watch", url }] : [],
         included_segments: ["Active Users", "Inactive Users"]
-    });
-    Client.sendNotification(notification)
-        .then(res => console.log(res.data))
-        .catch(err => console.log(err.data))
+    }))
+    sendQuee()
 }
 export default OneSignal
