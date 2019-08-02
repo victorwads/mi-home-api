8213ae51a5c69919c4e96ac8064bbd89be46ae41
Victor Cleber Laureano <victor.laureano@gmail.com>
2019-08-02 07:10:01 -0300

    Fix Lint

--- src/config/server.js
+++ src/config/server.js
@@ -1,27 +1,27 @@
 import cors from 'cors'
 
-const base_domain = process.env.BASE_DOMAIN
+const BASE_DOMAIN = process.env.BASE_DOMAIN
 const API_PORT = 8080
 const HOME_PORT = 8081
 
 var allowedOrigins = [
-  'https://' + base_domain,
-  'https://' + base_domain + ':7443',
-  'http://' + base_domain + ':' + API_PORT,
-  'http://' + base_domain + ':' + HOME_PORT
-];
+  'https://' + BASE_DOMAIN,
+  'https://' + BASE_DOMAIN + ':7443',
+  'http://' + BASE_DOMAIN + ':' + API_PORT,
+  'http://' + BASE_DOMAIN + ':' + HOME_PORT
+]
 
 export default {
   cors: api => {
     api.use(cors({
       origin: function (origin, callback) {
-        if (!origin) return callback(null, true);
+        if (!origin) return callback(null, true)
         if (allowedOrigins.indexOf(origin) === -1) {
-          return callback(null, false);
+          return callback(null, false)
         }
-        return callback(null, true);
+        return callback(null, true)
       }
-    }));
+    }))
   },
   start: (api, home) => {
     api.use((req, res, next) => {
